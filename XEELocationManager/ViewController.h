296968a288a3cf0534f5870a78b8589c11a76f9d
Andrija Cajic <ancajic@gmail.com>
2013-11-14 16:11:47 +0100

    Initial commit

---
+++ XEELocationManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XEELocationManager
+//
+//  Created by Andrija Cajic on 14/11/13.
+//  Copyright (c) 2013 Andrija Cajic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ XEELocationManager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XEELocationManager
+//
+//  Created by Andrija Cajic on 14/11/13.
+//  Copyright (c) 2013 Andrija Cajic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
